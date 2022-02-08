410741fd8c89a0b3bd4725c471e49a8f4a690d71
December <jiale.gfi@outlook.com>
2022-02-08 22:22:27 +0800

    init config

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import importlib
+
+
+ADDONS = [
+
+]
+
+
+def import_models():
+    for addon in ADDONS:
+        module_path = f'app.models.{addon}'
+        try:
+            importlib.import_module(module_path)
+        except ModuleNotFoundError:
+            raise RuntimeError(f"Not found {addon}")
+
+
+def create_app():
+
+    _app = Flask(__name__)
+
+    # import models
+    import_models()
+    
+
+    return _app
+
+
+app = create_app()
---
+++ app/settings.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+
+class Setting(object):
+
+    default = {
+
+    }
+
+    def __init__(self):
+        env_file_path = 'env.json'
+        if not os.path.exists(env_file_path):
+            raise RuntimeError(f'请在项目根目录下创建配置文件: {env_file_path}')
+
+        with open(env_file_path, 'r') as f:
+            envs: dict = json.load(f)
+
+        for key in self.default.keys():
+            value = envs.get(key, None)
+            if value is None:
+                value = os.environ.get(key, None)
+            if value is None:
+                value = self.default[key]
+
+            value = self._convert_type(key, value)
+
+            setattr(self, key, value)
+
+
+    def _convert_type(self, k, v):
+        default_value = self.default[k]
+        if default_value is None:
+            return v
+
+        type_ = type(default_value)
+        if type_ == bool:
+            v = int(v)
+
+        return type_(v)
+
+
+setting = Setting()
