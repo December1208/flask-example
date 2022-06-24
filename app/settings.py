2a7f75b966593148a75568ca1397517ecd03c312
December <jiale.gfi@outlook.com>
2022-06-24 16:07:11 +0800

    优化配置文件的定义

--- app/settings.py
+++ app/settings.py
@@ -1,54 +1,81 @@
 import json
 import os
+import dotenv
+from typing import Union
+
+
+dotenv.load_dotenv(verbose=True)
+
+
+def parse_str(key, default_value):
+    env_value = os.environ.get(key, default_value)
+    return env_value
+
+
+def parse_list(key, default_value):
+    env_value = os.environ.get(key, '')
+    value = env_value.split(',') or default_value
+    return value
+
+
+def parse_set(key, default_value):
+    return set(parse_list(key, default_value))
+
+
+def parse_dict(key, default_value):
+    env_value = os.environ.get(key, '{}')
+    value = json.loads(env_value) or default_value
+    return value
+
+
+def parse_bool(key, default_value):
+    return bool(parse_int(key, default_value))
+
+
+def parse_int(key, default_value):
+    env_value = os.environ.get(key)
+    if env_value is None:
+        return default_value
+    value = int(env_value)
+    return value
+
+
+def load_setting_value(field, field_type, default_value):
+    valid_field_type_to_parse_func = {
+        str: parse_str,
+        list: parse_list,
+        set: parse_set,
+        dict: parse_dict,
+        bool: parse_bool,
+        int: parse_int,
+    }
+
+    parse_func = valid_field_type_to_parse_func.get(field_type)
+    if parse_func is None:
+        raise Exception(
+            f"unsupported field type {field_type}, only support field type {valid_field_type_to_parse_func.keys()}"
+        )
+    value = parse_func(field, default_value)
+    return value
 
 
 class Setting(object):
 
-    default = {
-        'ENV': 'development',
-        'DEBUG': True,
-        'TESTING': False,
-        'SECRET_KEY': '6ea36d022bb44727be60ca22483fa22d999535294fd4922242b470a922e955c3',
-        'JWT_SECRET_KEY': 'ddfaac4a33c94806',
-        'JWT_TOKEN_LOCATION': ['headers'],
-        'JWT_ACCESS_TOKEN_EXPIRES': 60 * 5,
-        'JWT_REFRESH_TOKEN_EXPIRES': 60 * 60 * 24 * 7,
-        'SQLALCHEMY_DATABASE_URI': 'postgresql://lintcodesaas:lintcodesaas@localhost:5432/lintcode_saas',
-        'TEST_SQLALCHEMY_DATABASE_URI': 'postgresql://lintcodesaas:lintcodesaas@localhost:5432/test_lintcode_saas',
-        'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-        'SQLALCHEMY_ENGINE_OPTIONS': {'pool_size': 10, 'pool_recycle': 3600},
-        'TEST_DATA_PATH': 'sample_data',
-    }
-
-    def __init__(self):
-        env_file_path = 'env.json'
-        if not os.path.exists(env_file_path):
-            raise RuntimeError(f'请在项目根目录下创建配置文件: {env_file_path}')
-
-        with open(env_file_path, 'r') as f:
-            envs: dict = json.load(f)
-
-        for key in self.default.keys():
-            value = envs.get(key, None)
-            if value is None:
-                value = os.environ.get(key, None)
-            if value is None:
-                value = self.default[key]
-
-            value = self._convert_type(key, value)
-
-            setattr(self, key, value)
-
-    def _convert_type(self, k, v):
-        default_value = self.default[k]
-        if default_value is None:
-            return v
-
-        type_ = type(default_value)
-        if type_ == bool:
-            v = int(v)
-
-        return type_(v)
+    FLASK_ENV = load_setting_value('FLASK_ENV', str, 'production')
+    SECRET_KEY = load_setting_value('SECRET_KEY', str, '45008b73ad9d00c01f174dddd41df6ad')
+    TESTING = load_setting_value('TESTING', bool, False)
+    JWT_SECRET_KEY = load_setting_value('JWT_SECRET_KEY', str, 'ddfaac4a33c94806')
+    JWT_TOKEN_LOCATION = load_setting_value('JWT_TOKEN_LOCATION', list, ['headers'])
+    JWT_ACCESS_TOKEN_EXPIRES = load_setting_value('JWT_ACCESS_TOKEN_EXPIRES', int, 60 * 5)
+    JWT_REFRESH_TOKEN_EXPIRES = load_setting_value('JWT_REFRESH_TOKEN_EXPIRES', int, 60 * 60 * 24 * 7)
+    SQLALCHEMY_DATABASE_URI = load_setting_value(
+        'SQLALCHEMY_DATABASE_URI', str, 'postgresql://lintcodesaas:lintcodesaas@localhost:5432/lintcode_saas'
+    )
+    SQLALCHEMY_ENGINE_OPTIONS = load_setting_value(
+        'SQLALCHEMY_ENGINE_OPTIONS', dict, {'pool_size': 10, 'pool_recycle': 3600}
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = load_setting_value('SQLALCHEMY_TRACK_MODIFICATIONS', bool, False)
+    TEST_DATA_PATH = load_setting_value('TEST_DATA_PATH', str, 'sample_data')
 
 
 setting = Setting()
